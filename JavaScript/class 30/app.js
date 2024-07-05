f105c828334b3b214bd3c0fb914b72763094c0c9
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-07-05 09:21:06 +0500

    Update app.js

--- JavaScript/class 30/app.js
+++ JavaScript/class 30/app.js
@@ -6,17 +6,17 @@
 function login() {
     console.log("login()")
 
-    // var isMatch = true
+    var isMatch = true
 
-    // if (isMatch) {
-    //     // navigate home page to about page
-    //     // window.location.href = "./about.html"
-    //     // window.location.assign("./about.html")
-    //     // window.location.replace("./about.html")
-    //     window.location.reload()
-    // } else {
-    //     alert("email or password inCorrect")
-    // }
+    if (isMatch) {
+        // navigate home page to about page
+        // window.location.href = "./about.html"
+        // window.location.assign("./about.html")
+        // window.location.replace("./about.html")
+        window.location.reload()
+    } else {
+        alert("email or password inCorrect")
+    }
     window.open("./about.html" , "win1" , "width=250;height=250")
 
 
